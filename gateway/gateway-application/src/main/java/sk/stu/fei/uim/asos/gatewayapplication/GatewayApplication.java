cd174e9a8a14b65a0420799cbb2008bb2b18d0eb
dudakp <palodudak@gmail.com>
2022-10-30 13:36:21 +0100

    feature: added feign client for external api /w fallback

--- gateway/gateway-application/src/main/java/sk/stu/fei/uim/asos/gatewayapplication/GatewayApplication.java
+++ gateway/gateway-application/src/main/java/sk/stu/fei/uim/asos/gatewayapplication/GatewayApplication.java
@@ -5,12 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import sk.stu.fei.uim.asos.dto.req.AddRequestDto;
 import sk.stu.fei.uim.asos.dto.res.AddResponseDto;
 import sk.stu.fei.uim.asos.gatewayapplication.feign.AddServiceClient;
+import sk.stu.fei.uim.asos.gatewayapplication.feign.ExternalMathOperationsClient;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -20,9 +23,11 @@ public class GatewayApplication {
     private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
 
     private final AddServiceClient addServiceClient;
+    private final ExternalMathOperationsClient externalMathOperationsClient;
 
-    public GatewayApplication(AddServiceClient addServiceClient) {
+    public GatewayApplication(AddServiceClient addServiceClient, ExternalMathOperationsClient externalMathOperationsClient) {
         this.addServiceClient = addServiceClient;
+        this.externalMathOperationsClient = externalMathOperationsClient;
     }
 
     public static void main(String[] args) {
@@ -32,5 +37,10 @@ public class GatewayApplication {
     @PutMapping("/add")
     public AddResponseDto add(@RequestBody AddRequestDto req) {
         return addServiceClient.add(req);
+    }
+
+    @GetMapping("/trivia")
+    public String getTrivia(@RequestParam Long num) {
+        return externalMathOperationsClient.getTrivia(num);
     }
 }
